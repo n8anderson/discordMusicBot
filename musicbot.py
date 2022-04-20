b22914ef603c6546af810992d091c90d8517f724
n8anderson <bsuCompSci2021!>
2022-04-20 12:51:55 -0400

    Downloader and other commands

--- musicbot.py
+++ musicbot.py
@@ -60,6 +60,7 @@ class YTDLSource(ds.PCMVolumeTransformer):
 
         if 'entries' in data:
             # take first item from a playlist
+            print(data)
             data = data['entries'][0]
         filename = data['title'] if stream else ytdl.prepare_filename(data)
         return filename
@@ -89,3 +90,56 @@ async def leave(ctx):
         await voice_client.disconnect()
     else:
         await ctx.send("I am not connected to any voice channels.")
+
+
+@musicBot.command(name='play', help='Searches for and plays a song if found')
+async def play(ctx, url):
+    print(ctx.message)
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            fileName = await YTDLSource.from_url(url, loop=musicBot.loop)
+            voice_channel.play(ds.FFmpegPCMAudio(executable='bin\\ffmpeg.exe', source=fileName))
+        await ctx.send("**Now playing: {}**".format(fileName))
+    except Exception as E:
+        print(str(E))
+        await ctx.send("I am not connected to a voice channel.")
+
+
+@musicBot.command(name='pause', help='Pauses the current song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+
+    if voice_client.is_playing():
+        await voice_client.pause()
+        await ctx.send("Music paused.")
+    else:
+        await ctx.send("Nothing is playing at the moment.")
+
+
+@musicBot.command(name='resume', help='Resumes the current song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+
+    if voice_client.is_paused():
+        voice_client.resume()
+        await ctx.send("Music resumed.")
+    else:
+        await ctx.send('Nothing is paused or playing at the moment.')
+
+
+@musicBot.command(name='stop', help='Stops the current song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+
+    if voice_client.is_paused() or voice_client.is_playing():
+        voice_client.stop()
+        await ctx.send("Music stopped.")
+
+    else:
+        await ctx.send('I am doing nothing, what do you want from me.')
+
+
+musicBot.run(TOKEN)
